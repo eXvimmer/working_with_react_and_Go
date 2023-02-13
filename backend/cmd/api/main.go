c45b42ca0095c14408a9ea0cb96a6ba0e593e056
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-13 15:02:27 +0330

    backend: install pq and godotenv and connect to database.

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
@@ -28,13 +36,25 @@ type application struct {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := os.Getenv("DSN")
+
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "application environment (development|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", dsn, "postgres connection string")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -50,10 +70,24 @@ func main() {
 	}
 
 	logger.Println("🚀 starting server on port", cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 // psql --host=localhost --dbname=go_movies --username=postgres
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/exvimmer/working_with_react_and_go/backend
 
 go 1.19
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.7
+	github.com/joho/godotenv v1.5.1
+)
