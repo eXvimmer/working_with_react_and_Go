46865a63664c554e589d1d175dfc7fd730ae4756
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-14 20:16:01 +0330

    frontend: fix: change every occurance of categories to genres and create the Genres component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import { Route, Switch, Link } from "react-router-dom";
 import Home from "./routes/Home";
 import Movies from "./routes/Movies";
 import Admin from "./routes/Admin";
-import CategoryPage from "./routes/CategoryPage";
-import Categories from "./components/Categories";
+import Genres from "./routes/Genres";
 import Movie from "./components/Movie";
 
 function App() {
@@ -25,7 +24,7 @@ function App() {
                 <Link to="/movies">Movies</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/categories">Categories</Link>
+                <Link to="/genres">Genres</Link>
               </li>
               <li className="list-group-item">
                 <Link to="/admin">Admin</Link>
@@ -43,19 +42,9 @@ function App() {
             <Route exact path="/movies">
               <Movies />
             </Route>
-            <Route exact path="/categories">
-              <CategoryPage />
+            <Route exact path="/genres">
+              <Genres />
             </Route>
-            <Route
-              exact
-              path="/categories/drama"
-              render={(props) => <Categories {...props} title="Drama" />}
-            />
-            <Route
-              exact
-              path="/categories/comedy"
-              render={(props) => <Categories {...props} title="Comedy" />}
-            />
             <Route exact path="/admin">
               <Admin />
             </Route>
---
+++ frontend/src/routes/Genres.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Genres extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      genres: [],
+      isLoading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/v1/genres")
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error(`invalid response code: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({
+          genres: json.genres,
+          isLoading: false,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          isLoading: false,
+          error: err,
+        });
+      });
+  }
+
+  render() {
+    const { genres /*isLoading, error*/ } = this.state;
+
+    return (
+      <>
+        <h2>Genres</h2>
+
+        <ul>
+          {genres.map((g) => (
+            <li key={g.id}>
+              <Link to={`/genre/${g.id}`}>{g.genre_name}</Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
