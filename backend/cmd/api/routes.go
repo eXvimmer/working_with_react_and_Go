8eefee64bd01381f52f51160973e46344f3920f0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-14 14:43:02 +0330

    backend: enable CORS.

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -6,12 +6,12 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	r := httprouter.New()
 
 	r.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 	r.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	r.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 
-	return r
+	return app.enableCORS(r)
 }
