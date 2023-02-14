e21a0984034b9dc77fb8fd3d3d0c5134ce220f35
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-14 17:14:24 +0330

    frontend: display details of a specific movie.

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React, { Component } from "react";
 
 export class Movie extends Component {
@@ -7,46 +6,97 @@ export class Movie extends Component {
 
     this.state = {
       movie: {},
+      isLoading: true,
+      error: null,
     };
   }
 
   componentDidMount() {
-    this.setState({
-      movie: {
-        id: this.props.match.params.id,
-        title: "some movie",
-        runtime: 90,
-      },
-    });
+    fetch(`http://localhost:4000/v1/movie/${this.props.match.params.id}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error(`invalid response code: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({
+          movie: json.movie,
+          isLoading: false,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          isLoading: false,
+          error: err,
+        });
+      });
   }
 
   render() {
-    return (
-      <>
-        <h2>Movie: {this.state.movie.title}</h2>
-        <table className="table table-compact table-striped">
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td>
-                <strong>Title:</strong>
-              </td>
-              <td>
-                <strong>{this.state.movie.title}</strong>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <strong>Runtime:</strong>
-              </td>
-              <td>
-                <strong>{this.state.movie.runtime} minutes</strong>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </>
-    );
+    const { movie, isLoading, error } = this.state;
+
+    if (movie.genres) {
+      movie.genres = Object.values(movie.genres);
+    } else {
+      movie.genres = [];
+    }
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (isLoading) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <>
+          <h2>
+            Movie: {movie.title} ({movie.year})
+          </h2>
+          <div className="float-start">
+            <small>Rating: {movie.mpaa_rating}</small>
+          </div>
+          <div className="float-end">
+            {movie.genres.map((g, i) => (
+              <span className="badge bg-secondary me-1" key={i}>
+                {g}
+              </span>
+            ))}
+          </div>
+          <div className="clearfix"></div>
+          <hr />
+          <table className="table table-compact table-striped">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Title:</strong>
+                </td>
+                <td>
+                  <strong>{movie.title}</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Desciption:</strong>
+                </td>
+                <td>
+                  <strong>{movie.description}</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Run time:</strong>
+                </td>
+                <td>
+                  <strong>{movie.runtime} minutes</strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      );
+    }
   }
 }
 
