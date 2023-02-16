80a896013fe0706cfb64f011f0782187de596e97
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-16 12:03:35 +0330

    backend: create a route and handler to delete a movie.

--- backend/cmd/api/movie_handlers.go
+++ backend/cmd/api/movie_handlers.go
@@ -58,7 +58,27 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {}
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	err = app.models.DB.DeleteMovie(id)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	ok := jsonResp{
+		OK:      true,
+		Message: "deleted",
+	}
+	err = app.writeJSON(w, http.StatusOK, ok, "response")
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+}
 
 func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 	var payload MoviePayload
